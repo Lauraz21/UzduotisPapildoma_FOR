b6fbc51ae2354bd8758d6af97492ac32adf794fd
Laura <Laura@DESKTOP-BESSB6E>
2023-09-13 21:19:39 +0300

    Add project files.

---
+++ UzduotisPapildoma_FOR/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace UzduotisPapildoma_FOR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 9;
+            int mult = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    mult = i * j;
+                    Console.WriteLine($" {i} x {j} = {mult}");
+                }
+            }
+
+        }
+    }
+}
